0a10775f4aa9086c908690ccb51badb1b862f1e3
Lara <laraellenh@gmail.com>
2021-11-17 14:34:13 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+function add() {
+}
+function subtract() {
+
+}
+function multiply (){
+
+}
+function divide () {
+}
+function add(a, b){
+const sum=a+b
+return sum
+}
+function subtract(a, b) {
+    const sum=a-b
+    return sum
+}
+function multiply(a, b) {
+    const sum=a*b
+    return sum
+}
+function divide(a, b) {
+    const sum=a/b
+    return sum
+}
+function increment (n) {
+     n++
+     return n
+}
+function decrement (n) {
+    n--
+    return n
+}
+function makeInt (n) {
+return parseInt(n, 10)
+}
+function preserveDecimal (n) {
+return parseFloat (n)
+}
